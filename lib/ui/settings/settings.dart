6d31b7b16f9d8385093492b03651a2b4226b000c
jrebecchi <johann.corcuff@gmail.com>
2020-05-21 01:11:09 +0200

    Adding login & register screens

---
+++ lib/ui/settings/settings.dart
@@ -0,0 +1,259 @@
+import 'package:boilerplate/data/sharedpref/constants/preferences.dart';
+import 'package:boilerplate/routes.dart';
+import 'package:boilerplate/stores/form/form_store.dart';
+import 'package:boilerplate/utils/device/device_utils.dart';
+import 'package:boilerplate/utils/locale/app_localization.dart';
+import 'package:boilerplate/widgets/app_icon_widget.dart';
+import 'package:boilerplate/widgets/empty_app_bar_widget.dart';
+import 'package:boilerplate/widgets/progress_indicator_widget.dart';
+import 'package:boilerplate/widgets/textfield_widget.dart';
+import 'package:flushbar/flushbar_helper.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../stores/theme/theme_store.dart';
+
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  //text controllers:-----------------------------------------------------------
+  TextEditingController _userEmailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  //stores:---------------------------------------------------------------------
+  ThemeStore _themeStore;
+
+  //focus node:-----------------------------------------------------------------
+  FocusNode _passwordFocusNode;
+
+  //form key:-------------------------------------------------------------------
+  final _formKey = GlobalKey<FormState>();
+
+  //stores:---------------------------------------------------------------------
+  final _store = FormStore();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _passwordFocusNode = FocusNode();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    _themeStore = Provider.of<ThemeStore>(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      primary: true,
+      appBar: EmptyAppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  // body methods:--------------------------------------------------------------
+  Widget _buildBody() {
+    return Material(
+      child: Stack(
+        children: <Widget>[
+          MediaQuery.of(context).orientation == Orientation.landscape
+              ? Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: _buildLeftSide(),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: _buildRightSide(),
+                    ),
+                  ],
+                )
+              : Center(child: _buildRightSide()),
+          Observer(
+            builder: (context) {
+              return Visibility(
+                visible: _store.loading,
+                child: CustomProgressIndicatorWidget(),
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLeftSide() {
+    return SizedBox.expand(
+      child: Image.asset(
+        'assets/images/img_login.jpg',
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _buildRightSide() {
+    return Form(
+      key: _formKey,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              AppIconWidget(image: 'assets/icons/ic_appicon.png'),
+              SizedBox(height: 50.0),
+              _buildUserIdField(),
+              _buildPasswordField(),
+              _buildSignUpButton(),
+              _buildLogInButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUserIdField() {
+    return Observer(
+      builder: (context) {
+        return Container(
+            child: TextFieldWidget(
+                hint: AppLocalizations.of(context).translate('login_email'),
+                inputType: TextInputType.emailAddress,
+                icon: Icons.person,
+                iconColor:
+                    _themeStore.darkMode ? Colors.white70 : Colors.black54,
+                textController: _userEmailController,
+                inputAction: TextInputAction.next,
+                onChanged: (value) {
+                  _store.setUserId(_userEmailController.text);
+                },
+                onFieldSubmitted: (value) {
+                  FocusScope.of(context).requestFocus(_passwordFocusNode);
+                },
+                errorText: _store.formErrorStore.userEmail),
+            margin: const EdgeInsets.only(bottom: 0.0));
+      },
+    );
+  }
+
+  Widget _buildPasswordField() {
+    return Observer(
+      builder: (context) {
+        return TextFieldWidget(
+          hint:
+              AppLocalizations.of(context).translate('login_et_user_password'),
+          isObscure: true,
+          padding: EdgeInsets.only(top: 16.0),
+          icon: Icons.lock,
+          iconColor: _themeStore.darkMode ? Colors.white70 : Colors.black54,
+          textController: _passwordController,
+          focusNode: _passwordFocusNode,
+          errorText: _store.formErrorStore.password,
+          onChanged: (value) {
+            _store.setPassword(_passwordController.text);
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildLogInButton() {
+    return Container(
+        margin: EdgeInsets.only(top: 20.0),
+        child: Center(
+          child: FlatButton(
+            child: Text(
+              AppLocalizations.of(context)
+                  .translate('register_already_have_an_account'),
+              style: Theme.of(context)
+                  .textTheme
+                  .subtitle1
+                  .copyWith(color: Colors.blue[300]),
+            ),
+            onPressed: () async {
+              navigateToLogin(context);
+            },
+          ),
+        ));
+  }
+
+  Widget _buildSignUpButton() {
+    return Container(
+        margin: EdgeInsets.only(top: 20.0),
+        child: CupertinoButton(
+          child: Text(
+              AppLocalizations.of(context).translate('register_btn_sign_up'),
+              style: new TextStyle(color: Colors.white)),
+          color: Theme.of(context).buttonColor,
+          onPressed: () async {
+            if (_store.canLogin) {
+              DeviceUtils.hideKeyboard(context);
+              _store.login();
+            } else {
+              _showErrorMessage('Please fill in all fields');
+            }
+          },
+        ));
+  }
+
+  Widget navigateToHome(BuildContext context) {
+    SharedPreferences.getInstance().then((prefs) {
+      prefs.setBool(Preferences.is_logged_in, true);
+    });
+
+    Future.delayed(Duration(milliseconds: 0), () {
+      Navigator.of(context).pushNamedAndRemoveUntil(
+          Routes.home, (Route<dynamic> route) => false);
+    });
+
+    return Container();
+  }
+
+  Widget navigateToLogin(BuildContext context) {
+    Future.delayed(Duration(milliseconds: 0), () {
+      Navigator.of(context).pushNamedAndRemoveUntil(
+          Routes.login, (Route<dynamic> route) => false);
+    });
+
+    return Container();
+  }
+
+  // General Methods:-----------------------------------------------------------
+  _showErrorMessage(String message) {
+    Future.delayed(Duration(milliseconds: 0), () {
+      if (message != null && message.isNotEmpty) {
+        FlushbarHelper.createError(
+          message: message,
+          title: AppLocalizations.of(context).translate('home_tv_error'),
+          duration: Duration(seconds: 3),
+        )..show(context);
+      }
+    });
+
+    return SizedBox.shrink();
+  }
+
+  // dispose:-------------------------------------------------------------------
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is removed from the Widget tree
+    _userEmailController.dispose();
+    _passwordController.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
+  }
+}
